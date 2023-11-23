93721f094bbafcd12b9d023d9f9cd074a22e6e44
ui1 <ui1@fardup.com>
2023-11-23 18:36:29 +0330

    timer

--- src/screens/user/Quiz.js
+++ src/screens/user/Quiz.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, VStack, Center, Button, Text, HStack, Box, Progress, Avatar, Image } from "native-base";
+import { View, VStack, Center, Button, Text, HStack, Box, Progress, Avatar, Image, InfoIcon } from "native-base";
 import { useRoute } from "@react-navigation/core";
 import axios from "axios";
 import CountdownTimer from "./CountdownTimer";
@@ -13,23 +13,64 @@ const Quiz = () => {
   const [inGame, setInGame] = useState(0);
   const [finishGame, setFinishGame] = useState(0);
   const [qustion, setQustion] = useState(0);
+  const [answerOk, setAnswerOk] = useState(0);
+  const [point, setPoint] = useState(0);
+  const [count, setCount] = useState(0);
 
   const startGame = (e) => {
     let data = {
       playerId: 1,
       gameId: 1
     }
-if (e) {
+    if (e) {
+      setAnswerOk(true)
   data.qId = qustion.id
-    data.answer = e
+      data.answer = e
+      if (e == qustion.qustionId[0]) {
+        
+    let timer = 5
+    const myInterval = setInterval(function () {
+      
+      if (timer > 0) {
+        timer -= 1;
+        // count += 1;
+        setCount(prevCount => prevCount + 1)
+        // setDecriment(timer);
+      } else {
+        timer =0
+        clearInterval(myInterval);
+        // setCount(count);
+        
+      }
+    }, 80);
+      
+      
+
+    //  let contt = 0
+    //     const interval = setInterval(() => {
+    //       setCount(prevCount => prevCount + 1);
+    //       contt++
+    //       if (contt == 5 ) {
+    //         return () => clearInterval(interval);
+    //       }
+    //     }, 100);
+
+         
+          setAnswerOk(e)
+      }else setAnswerOk(e + 4)
 }
     axios.post('https://quiz.iran.liara.run/game/answer', data)
       .then(response => {
         console.log("ttttt6", response.data)
+        response.data.qustionId = "441324213242343231"
         if (response.data) {
-        setQustion(response.data)
-        setTimer(0)
-        setInGame(true)
+          setTimeout(() => {
+            setQustion(response.data)
+            setTimer(0)
+            setInGame(true)
+            setAnswerOk(false)
+            
+          }, 300);
         } else {
           setFinishGame(true)
         setInGame(false)
@@ -73,26 +114,25 @@ if (e) {
       : inGame ?
         <HStack w='full' h='full' justifyContent='center' space={2}>
          <Center w="80%" bg="secondary" rounded="md" >
-        {/* <Box w="100%" maxW="500">
-        <Progress bg="coolGray.100" _filledTrack={{
-        bg: "lime.500"
-      }} value={75} mx="4" />
-      </Box> */}
+            <View >
+              <Avatar> {count ? count : 0}</Avatar>
+            <InfoIcon color='yellow.500'  size='xs'      /> 
+     </View>
             
-            <View borderRadius={5} w="92%" minHeight={150} p={2} color='primary'  bg={pallete.text} alignItems="center" ml={4} mr={4}>
-              <Image borderRadius={5} w="70%" minHeight={85} p={2} color='primary' ml={4} mr={4}  source={{
-                     uri: require('../../../assets/photos/quiz-logo.png')
+            <View borderRadius={5} w="95%" minHeight={230} p={2} color='primary'  bg={pallete.text} alignItems="center" ml={4} mr={4}>
+              <Image borderRadius={5} w="45%" minHeight={100} p={2} color='primary' ml={4} mr={4}  source={{
+                     uri: require('../../../assets/photos/logo2.png')
               }} alt="Alternate Text" />
               
-            <Text fontSize={15} borderRadius={5} w="90%" minHeight={85} p={2} color='primary' ml={4} mr={4} bg={pallete.text} marginBottom={2} >
+            <Text fontSize={14} borderRadius={5} w="90%" minHeight={85} p={2} color='primary' ml={4} mr={4} bg={pallete.text} marginBottom={2} >
               {qustion.body} </Text>
             </View>
           <VStack  w="90%" >
-            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2} color='primary' onPress={()=>startGame(2)} m={2} bg={pallete.info} marginBottom={1} > {qustion.option2}</Text>
-            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2} color='primary' onPress={()=>startGame(1)} m={2} bg={pallete.info} marginBottom={1} > {qustion.option1}</Text>
+            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2} color='primary' onPress={()=>startGame(1)} m={2} bg={answerOk == 1 ?  pallete.success: answerOk == 5 ? pallete.danger: pallete.info} marginBottom={1} > {qustion.option1}</Text>
+            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2} color='primary' onPress={()=>startGame(2)} m={2} bg={answerOk == 2 ? pallete.success: answerOk == 6 ? pallete.danger: pallete.info} marginBottom={1} > {qustion.option2}</Text>
           
-            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2}  color='primary' onPress={()=>startGame(4)} m={2} bg={pallete.info} marginBottom={1} > {qustion.option4}</Text>
-            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2}  color='primary' onPress={()=>startGame(3)} m={2} bg={pallete.info} marginBottom={1} > {qustion.option3}</Text>
+            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2}  color='primary' onPress={()=>startGame(3)} m={2} bg={answerOk == 3 ? pallete.success: answerOk == 7 ? pallete.danger: pallete.info} marginBottom={1} > {qustion.option3}</Text>
+            <Text fontSize={15} textAlign="center" borderRadius={5} w="90%" p={2}  color='primary' onPress={()=>startGame(4)} m={2} bg={answerOk == 4  ? pallete.success: answerOk == 8 ? pallete.danger: pallete.info} marginBottom={1} > {qustion.option4}</Text>
           </VStack>
           </Center>
           <VStack space={1} alignItems='center' h='full' borderWidth={1} borderRadius={5} p={4} width='10%'>

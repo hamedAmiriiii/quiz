d15cb6cc73e74f91569ade64bd19a02fadfc92a1
ui1 <ui1@fardup.com>
2023-12-06 10:21:40 +0330

    xcxz

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
 import { Avatar, HStack, Icon, InfoIcon, Text, VStack, View } from 'native-base'
 import React from 'react'
 import pallete from '../utils/theme/pallete'
-import { Pressable } from 'react-native'
+import { Linking, Pressable } from 'react-native'
+import { api } from '../api'
+import useUser from '../hooks/useUser'
 
 
 
 export default function Header() {
+
+
+    const user = useUser()
+
+    const buy = () => {
+        api({ method:"post",
+            url: 'deposit', data: {
+                amount: 1000,
+                userId:user._id
+        } }).then(res => {
+            console.log("rrrrrrrrr", res);
+            Linking.openURL(res.data.url)
+          }).catch(err => console.log(err))
+    }
   return (
    
           <HStack background={pallete.text} p={2} borderRadius={5} w='full' justifyContent='space-between'>
           <VStack space={2}>
               
-              <Pressable>
+              <Pressable onPress={buy}>
                 <HStack space={2} alignItems='center'>
                   <InfoIcon color='yellow.500'  size='xs'      /> 
                   <Text fontSize={12} color='yellow.500'>25000</Text>
